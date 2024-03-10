0ca196a16146e263ce5075addc78bc9a85658086
Sounder Rajendran <sounderajendran@gmail.com>
2024-03-10 05:18:23 -0400

    Update autoWolfTimer.py
    --dur => -d flag changes
    try-except error handling for trust-browser page
    Warnings/non-fatal errors suppression

--- autoWolfTimer.py
+++ autoWolfTimer.py
@@ -3,6 +3,7 @@ import sys
 import time
 import argparse
 from selenium import webdriver
+from datetime import datetime, timedelta
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -13,16 +14,19 @@ from selenium.webdriver.support import expected_conditions as EC
 username_str = "" # Unity ID
 password_str = "" # Unity ID password
 
-webDriverDelay = 1000
+global_delay = 15 # seconds
 
 # scan if user_data dir is present and create if it's not there.
-if not os.path.exists("./user_path"):
-    os.makedirs("./user_path")
+if not os.path.exists("./user_data"):
+    os.makedirs("./user_data")
 
 chrome_options = Options()
 
 # launch chrome in headless mode
 chrome_options.add_argument("--headless=new")
+
+# supress all non-fatal warnings/errors
+chrome_options.add_argument('--log-level=3')
 
 # use ./user_data folder to store/retrieve user data (useful to save cache/cookies used for duo 2FA)
 s = "--user-data-dir={}".format(os.path.abspath(os.path.join(os.path.curdir, "user_data")))
@@ -31,23 +35,34 @@ chrome_options.add_argument(raw_s)
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Automate WolfTime clockin and clockout before and after a given duration.')
-parser.add_argument('hours', type=str, help='Duration in hours and minutes (e.g., 3.25 for 3 hours and 25 minutes)')
+parser.add_argument('-d', action='store_true', help='Flag to indicate if the duration is provided in total hours (e.g., 1.5 for 1 hour and 30 minutes)')
+parser.add_argument('hours', type=str, help='Duration in hours and minutes (e.g., 1.5 => 1 hour and 50 minutes (default) or if -d flag is used 1.5 => 1 hour and 30 minutes.)')
 args = parser.parse_args()
 
-# Convert hours and minutes to seconds
+# Convert hours and minutes to seconds and then to a timedelta
 try:
-    hours_part, mins_part = args.hours.split('.') # considering in format <hours>.<mins>
-    total_seconds = int(hours_part) * 3600 + int(mins_part) * 60
+    if args.d:
+        # If total_hours flag is used, interpret the input as total hours
+        hours_part = float(args.hours)
+        intended_delay = timedelta(hours=hours_part)
+        print("Input format: Total Hours")
+    else:
+        # Default to hours.minutes format
+        hours_part, mins_part = map(int, args.hours.split('.')) # considering in format <hours>.<mins>
+        intended_delay = timedelta(hours=hours_part, minutes=mins_part)
+        print("Input format: Hours and Minutes")
 except ValueError:
-    print("Error: Please provide the time in correct format (e.g., 3.25 for 3 hours and 25 minutes)")
+    print("Error: Please provide the time in correct format.")
     sys.exit(1)
 
-# make sure the total time can't be more than 4 hours, or else the accounts dept is gonna mail you again. if time is less than 2 mins, the portal is likely to throw a tantrum. 
-if total_seconds < 2 * 60 or total_seconds > 4 * 3600:
+# Check for the intended total time
+if intended_delay.total_seconds() < 2 * 60 or intended_delay.total_seconds() > 4 * 3600:
     print("Error: The duration must be between 0.02 and 4.00 hours.")
     sys.exit(1)
 
 def punch_in_or_out():
+    
+    sys.stdout.write("\a") # plays sound without printing a new line. 
 
     # Setup Chrome using webdriver-manager
     service = Service(ChromeDriverManager().install())
@@ -57,10 +72,10 @@ def punch_in_or_out():
     driver.get("https://portalsp.acs.ncsu.edu/Shibboleth.sso/Login?SAMLDS=1&target=ss%3Amem%3A0d279c93132bba229c8d7927f6e4910374cb4a668c6affb73803521b54774382&entityID=https%3A%2F%2Fshib.ncsu.edu%2Fidp%2Fshibboleth")
 
     # Wait for the username field to be present before proceeding
-    WebDriverWait(driver, webDriverDelay).until(
+    WebDriverWait(driver, global_delay).until(
         EC.presence_of_element_located((By.ID, "username"))
     )
-
+    
     # Find the username and password fields and fill them out
     username_field = driver.find_element(By.ID, "username")
     password_field = driver.find_element(By.ID, "password")
@@ -70,18 +85,22 @@ def punch_in_or_out():
     login_button = driver.find_element(By.ID, "formSubmit")
     login_button.click()
     
-    sys.stdout.write("\a") # plays sound without printing a new line, to alert the user of a possible 2FA Duo push. 
     # Hopefully, Duo 2FA will happen only once per Duo session (session length can be configured: https://duo.com/docs/policy#remembered-devices) as we store cookies.
     
     # After clicking login, we are greeted with a "Trust this device?" page. Ideally, we prefer not to receive 2FA prompt every time we log in. So we click the "This is my device" button.
-    WebDriverWait(driver, webDriverDelay).until(EC.presence_of_element_located((By.ID, 'trust-browser-button')))
-    trust_button = driver.find_element(By.ID, "trust-browser-button")
-    trust_button.click()
-
+    try:
+        WebDriverWait(driver, global_delay*2).until(EC.presence_of_element_located((By.ID, 'trust-browser-button')))
+        trust_button = driver.find_element(By.ID, "trust-browser-button")
+        trust_button.click()
+        print("2FA requested!")
+    except:
+        print("No 2FA requested!")
+        pass # we assume that cookies we're used to login without Duo push request. If this 'try' failed for someother reason, we'll get fatal errors in the subsequent sections.
+         
     # When mypack is loaded without cache/cookies it defaults to the first page in available forms which is "Employee self services" for all student employees.
     # since we don't navigate anywhere else from the context of this script, we can assume that we always will be routed to "Employee self services" page.
 
-    time.sleep(12) # This is a simple workaround for the page loading issue; I hope the portal loads in 12 seconds, if the connection is slower, adjust this delay.
+    time.sleep(global_delay) # This is a simple workaround for the page loading issue; I hope the portal loads in 15 seconds, if the connection is slower, adjust this delay.
     wolf_time_button = driver.find_element(By.ID, "win0divPTNUI_LAND_REC_GROUPLET$7")
     driver.execute_script("arguments[0].click();", wolf_time_button)
 
@@ -89,12 +108,12 @@ def punch_in_or_out():
     # First, switch to the iframe by its ID
     time.sleep(2)
     try:
-        WebDriverWait(driver, webDriverDelay).until(EC.presence_of_element_located((By.ID, 'win0groupletPTNUI_LAND_REC_GROUPLET$0_iframe')))
+        WebDriverWait(driver, global_delay*2).until(EC.presence_of_element_located((By.ID, 'win0groupletPTNUI_LAND_REC_GROUPLET$0_iframe')))
         iframe_id = "win0groupletPTNUI_LAND_REC_GROUPLET$0_iframe"
         driver.switch_to.frame(iframe_id)
 
         # Now that you're within the iframe, you can find and interact with the element
-        link = WebDriverWait(driver, webDriverDelay).until(
+        link = WebDriverWait(driver, global_delay*2).until(
             EC.element_to_be_clickable((By.ID, "TL_RPTD_SFF_WK_TL_PUNCH_1"))
         )
         link.click()
@@ -105,14 +124,23 @@ def punch_in_or_out():
         time.sleep(2)
         driver.quit()
 
+# Calculate the wake time
+wake_time = datetime.now() + intended_delay
+
+print("\n---------------------------------------Duration set for: {} hours and {} minutes----------------------------------------".format(intended_delay.total_seconds() // 3600, (intended_delay.total_seconds() % 3600) // 60))
 punch_in_or_out()
 
-print("WolfTime Clock In performed. Waiting for {:.2f} hours...".format(total_seconds / 3600))
+print("\n---------------------------------------WolfTime Clock In performed. Waiting until {}---------------------".format(wake_time.strftime('%Y-%m-%d %H:%M:%S')))
 
-time.sleep(total_seconds)
+# Loop until the current time is greater than or equal to the wake time
+# We're checking for elapsed time this way as our program can run for at most 4 hrs and in this time if the activity is paused or the core is in CC6 mode (cause of laptop sleep?) 
+# the time.sleep() alone wouldn't be enough to keep track of time elapsed.
 
-print("Time Elapsed. Performing WolfTime Clock Out...")
+while datetime.now() < wake_time:
+    time.sleep(global_delay)  # Check every 'global_delay' seconds.
+
+print("\n---------------------------------------Time Elapsed. Performing WolfTime Clock Out----------------------------------------")
 
 punch_in_or_out()
 
-print("WolfTime Clock Out Successful")
+print("\n---------------------------------------WolfTime Clock Out Successful------------------------------------------------------")
