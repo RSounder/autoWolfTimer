dde0c172f6cc0496d34e968bd16a206ee07b59af
Sounder Rajendran <sounderajendran@gmail.com>
2024-03-10 03:42:31 -0400

    Update autoWolfTimer.py
    modified chrome launch to headless mode, and enabled cookie storage for saving 2FA auth info for reducing Duo push frequency.

--- autoWolfTimer.py
+++ autoWolfTimer.py
@@ -1,6 +1,7 @@
-import argparse
+import os
 import sys
 import time
+import argparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -8,52 +9,49 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
 username_str = "" # Unity ID
 password_str = "" # Unity ID password
 
 webDriverDelay = 1000
 
-# If you want you can suppress some minor warnings/logs/errors you can uncomment below
-# chrome_options = Options()
-# chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])  # This line helps to suppress the DevTools messages
+# scan if user_data dir is present and create if it's not there.
+if not os.path.exists("./user_path"):
+    os.makedirs("./user_path")
+
+chrome_options = Options()
+
+# launch chrome in headless mode
+chrome_options.add_argument("--headless=new")
+
+# use ./user_data folder to store/retrieve user data (useful to save cache/cookies used for duo 2FA)
+s = "--user-data-dir={}".format(os.path.abspath(os.path.join(os.path.curdir, "user_data")))
+raw_s = r'{}'.format(s)
+chrome_options.add_argument(raw_s)
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Automate WolfTime clockin and clockout before and after a given duration.')
-parser.add_argument('hours', type=str, help='Duration in hours and minutes (e.g., 1.5 => 1 hour and 50 minutes (default) or if --dur flag is used 1.5 => 1 hour and 30 minutes.)')
-parser.add_argument('--dur', action='store_true', help='Flag to indicate if the duration is provided in total hours (e.g., 1.5 for 1 hour and 30 minutes)')
+parser.add_argument('hours', type=str, help='Duration in hours and minutes (e.g., 3.25 for 3 hours and 25 minutes)')
 args = parser.parse_args()
 
 # Convert hours and minutes to seconds
 try:
-    if args.dur:
-        # If the total_hours flag is used, interpret the input as total hours
-        hours_part = float(args.hours)
-        mins_part = (hours_part - int(hours_part)) * 60
-        total_seconds = int(hours_part) * 3600 + int(mins_part) * 60
-        print("Input format: Total Hours")
-    else:
-        # Default to hours.minutes format
-        hours_part, mins_part = args.hours.split('.') # considering in format <hours>.<mins>
-        total_seconds = int(hours_part) * 3600 + int(mins_part) * 60
-        print("Input format: Hours and Minutes")
+    hours_part, mins_part = args.hours.split('.') # considering in format <hours>.<mins>
+    total_seconds = int(hours_part) * 3600 + int(mins_part) * 60
 except ValueError:
-    print("Error: Please provide the time in correct format.")
+    print("Error: Please provide the time in correct format (e.g., 3.25 for 3 hours and 25 minutes)")
     sys.exit(1)
 
-# make sure the total time can't be more than 4 hours, or else Bianca is gonna mail you again. if time is less than 2 mins, the portal will likely throw a tantrum. 
+# make sure the total time can't be more than 4 hours, or else the accounts dept is gonna mail you again. if time is less than 2 mins, the portal is likely to throw a tantrum. 
 if total_seconds < 2 * 60 or total_seconds > 4 * 3600:
     print("Error: The duration must be between 0.02 and 4.00 hours.")
-    sys.exit(1)    
+    sys.exit(1)
 
 def punch_in_or_out():
-    
-    sys.stdout.write("\a") # plays a sound to alert us without printing a new line # works only in cmd promt(Windows)/terminal(Unix)
-    
+
     # Setup Chrome using webdriver-manager
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     
     # Navigate to the login page. Now you may think that this doesn't look like 'mypack.ncsu.edu', and you'd be right. This page gets called when we try to log in to mypack and since selenium is not gonna store any cache or cookies, we always need to sign in. 
     driver.get("https://portalsp.acs.ncsu.edu/Shibboleth.sso/Login?SAMLDS=1&target=ss%3Amem%3A0d279c93132bba229c8d7927f6e4910374cb4a668c6affb73803521b54774382&entityID=https%3A%2F%2Fshib.ncsu.edu%2Fidp%2Fshibboleth")
@@ -72,14 +70,16 @@ def punch_in_or_out():
     login_button = driver.find_element(By.ID, "formSubmit")
     login_button.click()
     
-    sys.stdout.write("\a") # plays sound without printing new line
+    sys.stdout.write("\a") # plays sound without printing a new line, to alert the user of a possible 2FA Duo push. 
+    # Hopefully, Duo 2FA will happen only once per Duo session (session length can be configured: https://duo.com/docs/policy#remembered-devices) as we store cookies.
     
-    # After clicking login, we are greeted with a "Trust this device?" page. It doesn't matter which button we click; here we click the 'I trust' button, saying this is your device, not some shared system.
+    # After clicking login, we are greeted with a "Trust this device?" page. Ideally, we prefer not to receive 2FA prompt every time we log in. So we click the "This is my device" button.
     WebDriverWait(driver, webDriverDelay).until(EC.presence_of_element_located((By.ID, 'trust-browser-button')))
     trust_button = driver.find_element(By.ID, "trust-browser-button")
     trust_button.click()
 
-    # When mypack is loaded without cache/cookies it defaults to the first page in available forms which are "Employee self-services" for all student employees.
+    # When mypack is loaded without cache/cookies it defaults to the first page in available forms which is "Employee self services" for all student employees.
+    # since we don't navigate anywhere else from the context of this script, we can assume that we always will be routed to "Employee self services" page.
 
     time.sleep(12) # This is a simple workaround for the page loading issue; I hope the portal loads in 12 seconds, if the connection is slower, adjust this delay.
     wolf_time_button = driver.find_element(By.ID, "win0divPTNUI_LAND_REC_GROUPLET$7")
