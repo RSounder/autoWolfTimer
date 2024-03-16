b488c3f44ae98f18b5536149ea8d6dd52e84a88e
RSounder <srajend4@ncsu.edu>
2024-03-16 19:33:53 -0400

    added dyn support for wolftime and IN/OUT button

--- autoWolfTimer.py
+++ autoWolfTimer.py
@@ -5,6 +5,7 @@ import argparse
 from selenium import webdriver
 from datetime import datetime, timedelta
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,16 +15,14 @@ from selenium.webdriver.support import expected_conditions as EC
 username_str = "" # Unity ID
 password_str = "" # Unity ID password
 
-global_delay = 15 # seconds
+global_delay = 10 # seconds
+clockAction = "IN"
 
 # scan if user_data dir is present and create if it's not there.
 if not os.path.exists("./user_data"):
     os.makedirs("./user_data")
 
 chrome_options = Options()
-
-# launch chrome in headless mode
-chrome_options.add_argument("--headless=new")
 
 # supress all non-fatal warnings/errors
 chrome_options.add_argument('--log-level=3')
@@ -36,8 +35,13 @@ chrome_options.add_argument(raw_s)
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Automate WolfTime clockin and clockout before and after a given duration.')
 parser.add_argument('-d', action='store_true', help='Flag to indicate if the duration is provided in total hours (e.g., 1.5 for 1 hour and 30 minutes)')
+parser.add_argument('-w', action='store_true', help='Flag to indicate if chrome is to be invoked in windowed mode. (eg. default is headless, -w means windowed mode for chrome)')
 parser.add_argument('hours', type=str, help='Duration in hours and minutes (e.g., 1.5 => 1 hour and 50 minutes (default) or if -d flag is used 1.5 => 1 hour and 30 minutes.)')
 args = parser.parse_args()
+
+if not args.w:
+    # launch chrome in headless mode
+    chrome_options.add_argument("--headless=new")
 
 # Convert hours and minutes to seconds and then to a timedelta
 try:
@@ -101,33 +105,74 @@ def punch_in_or_out():
     # since we don't navigate anywhere else from the context of this script, we can assume that we always will be routed to "Employee self services" page.
 
     time.sleep(global_delay) # This is a simple workaround for the page loading issue; I hope the portal loads in 15 seconds, if the connection is slower, adjust this delay.
-    wolf_time_button = driver.find_element(By.ID, "win0divPTNUI_LAND_REC_GROUPLET$7")
-    driver.execute_script("arguments[0].click();", wolf_time_button)
+
+    # to select wolfTime button, we cant take grouplet number as its dynamic. so we search using inner text
+    # Use JavaScript to find and click the element with the text "WolfTime"
+    javascript = """
+    var elements = document.querySelectorAll('div.ps_box-group');
+    for (var i = 0; i < elements.length; i++) {
+        var span = elements[i].querySelector('span.ps-label');
+        if (span && span.textContent === 'WolfTime') {
+            elements[i].click(); // Click the parent div that contains the 'WolfTime' span
+            break; // Exit the loop once the element is found and clicked
+        }
+    }
+    """
+    driver.execute_script(javascript)
 
     # Inside wolfTime, the buttons are arranged within div via iframe. So we can't just search for it by id and click.
     # First, switch to the iframe by its ID
     time.sleep(2)
-    try:
-        WebDriverWait(driver, global_delay*2).until(EC.presence_of_element_located((By.ID, 'win0groupletPTNUI_LAND_REC_GROUPLET$0_iframe')))
-        iframe_id = "win0groupletPTNUI_LAND_REC_GROUPLET$0_iframe"
-        driver.switch_to.frame(iframe_id)
 
-        # Now that you're within the iframe, you can find and interact with the element
-        link = WebDriverWait(driver, global_delay*2).until(
-            EC.element_to_be_clickable((By.ID, "TL_RPTD_SFF_WK_TL_PUNCH_1"))
-        )
-        link.click()
+    WebDriverWait(driver, global_delay*2).until(EC.presence_of_element_located((By.ID, 'win0groupletPTNUI_LAND_REC_GROUPLET$0_iframe')))
+    iframe_id = "win0groupletPTNUI_LAND_REC_GROUPLET$0_iframe"
+    driver.switch_to.frame(iframe_id)
+    
+    threeDots = WebDriverWait(driver, global_delay*2).until(
+        EC.element_to_be_clickable((By.ID, "TL_RPTD_SFF_WK_GROUPBOX$PIMG"))
+    )
+    threeDots.click()
 
-    finally:
-        # Once done with actions within the iframe, switch back to the default content
-        driver.switch_to.default_content() 
-        time.sleep(2)
-        driver.quit()
+    viewFullSite = WebDriverWait(driver, global_delay*2).until(
+        EC.element_to_be_clickable((By.ID, "TL_TIME_TILE_WK_VIEW_ALL"))
+    )
+    viewFullSite.click()
+
+    # Once done with actions within the iframe, switch back to the default content
+    driver.switch_to.default_content() 
+    
+    WebDriverWait(driver, global_delay*2).until(EC.presence_of_element_located((By.ID, 'TL_RPTD_TIME_PUNCH_TYPE$0')))
+    
+    # In full site, there's a drop down box, from which in/out is to be selected. 
+    # <select id="TL_RPTD_TIME_PUNCH_TYPE$0" class="ps-dropdown" ...>
+    # <option value="">&nbsp; </option>    # <option value="1">In</option>    # <option value="2" selected="selected">Out</option>
+    # </select>
+
+    dropdown_element = driver.find_element(By.ID, "TL_RPTD_TIME_PUNCH_TYPE$0")
+    select = Select(dropdown_element)
+
+    if clockAction=="IN":
+        select.select_by_value("1")
+    else:
+        select.select_by_value("2")
+
+    # click submit button; id="TL_WEB_CLOCK_WK_TL_SAVE_PB"
+    submit = WebDriverWait(driver, global_delay).until(
+        EC.element_to_be_clickable((By.ID, "TL_WEB_CLOCK_WK_TL_SAVE_PB"))
+    )
+    submit.click()
+
+    # PT_CONFIRM_MSG
+    WebDriverWait(driver, global_delay).until(EC.presence_of_element_located((By.ID, 'PT_CONFIRM_MSG')))
+
+    time.sleep(2)
+    driver.quit()
 
 # Calculate the wake time
 wake_time = datetime.now() + intended_delay
 
 print("\n---------------------------------------Duration set for: {} hours and {} minutes----------------------------------------".format(intended_delay.total_seconds() // 3600, (intended_delay.total_seconds() % 3600) // 60))
+clockAction = "IN"
 punch_in_or_out()
 
 print("\n---------------------------------------WolfTime Clock In performed. Waiting until {}---------------------".format(wake_time.strftime('%Y-%m-%d %H:%M:%S')))
@@ -141,6 +186,7 @@ while datetime.now() < wake_time:
 
 print("\n---------------------------------------Time Elapsed. Performing WolfTime Clock Out----------------------------------------")
 
+clockAction = "OUT"
 punch_in_or_out()
 
 print("\n---------------------------------------WolfTime Clock Out Successful------------------------------------------------------")
